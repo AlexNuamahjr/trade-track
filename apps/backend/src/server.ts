a3d51592d7b04798c3d123ea8c3ae2661910b684
AlexNuamahjr <alexandernuamah@gmail.com>
2025-08-18 00:49:52 +0000

    Initialize backend application with Express, TypeScript, and Prisma setup; add configuration files and dependencies

---
+++ apps/backend/src/server.ts
@@ -0,0 +1,10 @@
+import app from "./app";
+import {logger} from "./utils"
+const PORT = 7890
+const startServer = ()=>{
+    app.listen(PORT, ()=>{
+        logger.info(`Server is running on http://localhost:${PORT}`)
+    })
+}
+
+startServer()
