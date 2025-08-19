3f65b93e8afb8be021e996cdd3a1e7851439d2eb
AlexNuamahjr <alexandernuamah@gmail.com>
2025-08-19 23:50:19 +0000

    Add request validation middleware using Zod for schema validation

---
+++ apps/backend/src/middlewares/validate.middlewar.ts
@@ -0,0 +1,31 @@
+import { Request, Response, NextFunction } from 'express'
+import { ZodAny } from 'zod'
+import { errorResponse,logger } from '../utils'
+import { StatusCodes } from 'http-status-codes'
+
+
+export const validate = (schema: ZodAny, schemaType: 'body' | 'params' | 'query' = 'body') => {
+   return (req: Request, res: Response, next: NextFunction):void => {
+        try {
+            logger.info({ message: 'Validating request', body: req.body, query: req.query, params: req.params })
+            switch (schemaType) {
+                case 'body':
+                    req.body = schema.parse(req.body)
+                    break
+                case 'params':
+                    req.params = schema.parse(req.params)
+                    break
+                case 'query':
+                    req.query = schema.parse(req.query)
+                    break
+                default:
+                    throw new Error('Invalid schema type')
+            }
+            next()
+        } catch (error: any) {
+            errorResponse(res, StatusCodes.BAD_REQUEST, 'Validation failed', error)
+        }
+
+    }
+
+}
